d8fdf23ec5adda0e13145d9a6fcc12d455601146
harendraprajapati72 <harendraprajapati72@gmail.com>
2021-11-15 00:25:54 +0530

    java Mathod

---
+++ MathodUse.java
@@ -0,0 +1,29 @@
+public class MathodUse {
+    public static void main(String[] args) {
+        System.out.println("this is class ");
+    }
+    
+}
+
+class MathodUse1{
+    public static void main(String[] args) {
+        System.out.println("this is class 1");
+    }
+    
+    
+}
+class MathodUse2 {
+    public static void main(String[] args) {
+        System.out.println("this is class 2");
+    }
+    
+    
+}
+
+class MathodUse3{
+    public static void main(String[] args) {
+        System.out.println("this is class 3");
+    }
+    
+    
+}
